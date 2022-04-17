60ea5e8ed7a5d51a9f4fb0300b6ad0a01e76d6d1
Frank Snow <fsnow@fsnow.com>
2022-04-16 23:55:30 -0400

    Incremented version, updated Lucene.net link in About box

--- src/Cst4/Properties/AssemblyInfo.cs
+++ src/Cst4/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.1.0.3")]
-[assembly: AssemblyFileVersion("4.1.0.3")]
+[assembly: AssemblyVersion("4.1.0.4")]
+[assembly: AssemblyFileVersion("4.1.0.4")]
